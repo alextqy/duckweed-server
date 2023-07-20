5d753501513e598dc67c3c685b7c66a8c9f85144
alextqy <289959263@qq.com>
2023-07-20 15:54:00 +0800

    coding

---
+++ Server/Api/BaseApi.go
@@ -0,0 +1,56 @@
+package api
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func GetMap(r *http.Request) map[string][]string {
+	return r.Form
+}
+
+func PostMap(r *http.Request) map[string][]string {
+	return r.PostForm
+}
+
+func Get(r *http.Request, key string) string {
+	return r.URL.Query().Get(key)
+}
+
+func Post(r *http.Request, key string) string {
+	return r.PostFormValue(key)
+}
+
+func FormFile(w http.ResponseWriter, r *http.Request, key string) (bool, string) {
+	f, fheader, err := r.FormFile("file")
+	defer func(f io.Closer) {
+		if err := f.Close(); err != nil {
+			fmt.Printf("defer close file err: %v", err.Error())
+		}
+	}(f)
+	if err != nil {
+		return false, err.Error()
+	}
+
+	newf, err := os.OpenFile("./Temp/upload/"+fheader.Filename, os.O_WRONLY|os.O_CREATE, 0666)
+	if err != nil {
+		return false, err.Error()
+	}
+	defer func(f io.Closer) {
+		if err := f.Close(); err != nil {
+			fmt.Printf("defer close file err: %v", err.Error())
+		}
+	}(newf)
+	if err != nil {
+		return false, err.Error()
+	}
+
+	_, err = io.Copy(newf, f)
+	if err != nil {
+		return false, err.Error()
+	}
+
+	return true, newf.Name()
+}
--- Server/main.go
+++ Server/main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	api "duckweed-server/Server/Api"
+	"log"
+	"net/http"
+	"time"
+)
 
+func main() {
+	port := "8000"
+	mux := http.NewServeMux()
+	routes(mux)
+	server := &http.Server{
+		Addr:         ":" + port,
+		WriteTimeout: time.Second * 5, //设置写超时
+		ReadTimeout:  time.Second * 5, //设置读超时
+		Handler:      mux,
+	}
+	log.Println("http server on port:" + port)
+	log.Fatal(server.ListenAndServe())
+}
+
+func routes(mux *http.ServeMux) {
+	mux.HandleFunc("/test", api.Test)
 }
