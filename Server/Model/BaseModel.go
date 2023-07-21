daf8bd4f32fb4000d481d2f7966c920f2ac5d2c4
alextqy <289959263@qq.com>
2023-07-21 16:39:14 +0800

    coding

--- Server/Model/BaseModel.go
+++ Server/Model/BaseModel.go
@@ -1,1 +1,16 @@
 package model
+
+import (
+	"database/sql"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func ConnDB() (bool, string, *sql.DB) {
+	//打开数据库，如果不存在，则创建
+	db, err := sql.Open("sqlite3", "../Dao.db")
+	if err != nil {
+		return false, err.Error(), nil
+	}
+	return true, "", db
+}
