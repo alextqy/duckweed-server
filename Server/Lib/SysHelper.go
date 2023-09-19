319e8dd277f35ec130801d38143eb09dab72ad64
alextqy <289959263@qq.com>
2023-09-19 17:02:38 +0800

    coding

--- Server/Lib/SysHelper.go
+++ Server/Lib/SysHelper.go
@@ -244,7 +244,7 @@ func RegEnNum(s string) bool {
 	return r.MatchString(s)
 }
 
-// 中英文 数字 下划线 短横线 中英文(逗号 句号 分号 感叹号)
+// 中英文 数字 下划线 短横线 中英文(逗号 句号 分号 感叹号 换行符 任何空白字符)
 func RegWriting(s string) bool {
 	r, err := regexp.Compile("^[\u4e00-\u9fa5_a-zA-Z0-9-,.;!，。；！\\n\\s]+$")
 	if err != nil {
